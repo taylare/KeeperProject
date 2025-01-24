7a53676f76a5f452e7ee3bc8777db65f83863b2f
Tayla Rechichi <tay.rechichi32@gmail.com>
2025-01-23 18:33:16 -0800

    Set up initial React app with components: Header, Footer, and Note

---
+++ src/components/App.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+
+function App(){
+    return (<div>
+        <Header />
+        <Note />
+        <Footer />
+    </div>);
+}
+
+/* function Heading(){
+    return <Header />;
+}
+
+function Foot(){
+    return <Footer />;
+}
+
+function Notes(){
+    return <Note />
+}
+*/
+export default App;
---
+++ src/components/Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note(){
+    return (
+        <note>
+            <h1>Title</h1>
+            <p>paragraph</p>
+        </note>
+    )
+}
+
+export default Note;
