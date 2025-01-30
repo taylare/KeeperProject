bd00d034141db8823768c7ea184b037d70d60168
Tayla Rechichi <tay.rechichi32@gmail.com>
2025-01-29 19:08:17 -0800

    - Updated  component to accept  and  as props. - Modified  to map through  array and render multiple  components. - Added unique  prop for optimized React rendering.

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 
-function Note(){
+function Note(props){
     return (
-        <note>
-            <h1>Title</h1>
-            <p>paragraph</p>
-        </note>
+        <div className="note">
+            <h1>{props.title}</h1>
+            <p>{props.content}</p>
+        </div>
     )
 }
 
